6ac3df55fc6b4d556c8ab360d270535c9c261860
Justin Kaufman <akaritakai@gmail.com>
2020-09-02 18:06:44 -0700

    WIP For normal law pitch pid controller

--- WASM/A32NX.cpp
+++ WASM/A32NX.cpp
@@ -2,6 +2,77 @@
 #include <MSFS/MSFS_Render.h>
 #include <MSFS/Legacy/gauges.h>
 #include <SimConnect.h>
+#include <cmath>
+#include <complex>
+
+/*
+ * ============== *
+ * PID CONTROLLER *
+ * ============== *
+ */
+constexpr double sign(double value)
+{
+	return (value > 0) ? 1.0 : -1.0;
+}
+class PIDController
+{
+public:
+	PIDController(const double output_min, const double output_max, const double Kp, const double Ki, const double Kd)
+		: output_min(output_min), output_max(output_max),
+		  Kp(Kp), Kd(Kd), Ki(Ki),
+		  integral(0),
+		  last_error(0), last_output(0)  {};
+	double Update(const double error, const double dt)
+	{
+		// Proportional term
+		double P = Kp * error;
+
+		// Integral term
+		double I;
+		//if (sign(error) == sign(last_output) && (last_output <= output_min || last_output >= output_max))
+		//{
+			// Clamp integral
+		//	I = 0;
+		//}
+		//else
+		//{
+			integral += error * dt;
+			I = Ki * integral;
+		//}
+
+		// Derivative term
+		double D = Kd * ((error - last_error) / dt);
+
+		double output = P + I + D;
+		// Clamp output
+		if (output < output_min)
+		{
+			output = output_min;
+		}
+		else if (output > output_max)
+		{
+			output = output_max;
+		}
+
+		// Save terms
+		last_output = output;
+		last_error = error;
+
+		printf("Error: %lf, Integral: %lf\n", error, integral);
+		return output;
+	}
+	void Reset()
+	{
+		integral = 0;
+		last_error = 0;
+		last_output = 0;
+	}
+private:
+	double output_min, output_max;
+	double Kp, Kd, Ki;
+	double integral;
+	double last_error, last_output;
+};
 
 /*
  * ========================== *
@@ -15,8 +86,13 @@ struct SIM_VARS
 	ENUM degrees_units; // Degrees
 	ENUM feet_units; // Feet
 	ENUM gforce_units; // GForce
+	ENUM percent_units; // Percent
+	ENUM radians_units; // Radians
 	// Variables
+	ENUM elevator_deflection; // ELEVATOR DEFLECTION
+	ENUM elevator_deflection_pct; // ELEVATOR DEFLECTION PCT
 	ENUM gforce; // G FORCE
+	ENUM plane_bank_degrees; // PLANE BANK DEGREES
 	ENUM plane_pitch_degrees; // PLANE PITCH DEGREES
 	ENUM radio_height; // RADIO HEIGHT
 	ENUM sim_on_ground; // SIM ON GROUND
@@ -33,8 +109,13 @@ bool HandleSimVars(FsContext ctx, int service_id, void* pData)
 		sim_vars.degrees_units = get_units_enum("Degrees");
 		sim_vars.feet_units = get_units_enum("Feet");
 		sim_vars.gforce_units = get_units_enum("GForce");
+		sim_vars.percent_units = get_units_enum("Percent");
+		sim_vars.radians_units = get_units_enum("Radians");
 		// Variables
+		sim_vars.elevator_deflection = get_aircraft_var_enum("ELEVATOR DEFLECTION");
+		sim_vars.elevator_deflection_pct = get_aircraft_var_enum("ELEVATOR DEFLECTION PCT");
 		sim_vars.gforce = get_aircraft_var_enum("G FORCE");
+		sim_vars.plane_bank_degrees = get_aircraft_var_enum("PLANE BANK DEGREES");
 		sim_vars.plane_pitch_degrees = get_aircraft_var_enum("PLANE PITCH DEGREES");
 		sim_vars.radio_height = get_aircraft_var_enum("RADIO HEIGHT");
 		sim_vars.sim_on_ground = get_aircraft_var_enum("SIM ON GROUND");
@@ -72,13 +153,18 @@ bool HandleSimConnect(FsContext ctx, int service_id, void* pData)
 	{
 	case PANEL_SERVICE_PRE_INSTALL:
 	{
-		hSimConnect = SimConnect_Open(&hSimConnect, "A32NX", nullptr, 0, 0, 0);
 		return true;
 	}
 	break;
 	case PANEL_SERVICE_POST_INSTALL:
 	{
-		return true;
+		if (SUCCEEDED(SimConnect_Open(&hSimConnect, "A32NX", nullptr, 0, 0, 0)))
+		{
+			printf("A32NX: Connected via SimConnect\n");
+			return true;
+		}
+		printf("A32NX: Failed to connect via SimConnect\n");
+		return false;
 	}
 	break;
 	case PANEL_SERVICE_PRE_DRAW:
@@ -88,7 +174,11 @@ bool HandleSimConnect(FsContext ctx, int service_id, void* pData)
 	break;
 	case PANEL_SERVICE_PRE_KILL:
 	{
-		SimConnect_Close(hSimConnect);
+		if (hSimConnect)
+		{
+			SimConnect_Close(hSimConnect);
+			hSimConnect = 0;
+		}
 		return true;
 	}
 	break;
@@ -296,19 +386,16 @@ bool HandlePitchControlMode(FsContext ctx, int service_id, void* pData)
 		// - The t member gives the absolute simulation time.
 		// - The dt member gives the time elapsed since last frame.
 		auto* p_draw_data = static_cast<sGaugeDrawData*>(pData);
-		auto current_sim_time = p_draw_data->t; // Assuming this is seconds	
+		auto current_sim_time = p_draw_data->t;
 		switch (pitch_control_mode)
 		{
 		case GROUND_MODE:
-			printf("CURRENT MODE: GROUND MODE\n");
 			PitchControlMode_HandleGroundTransitions(current_sim_time);
 			break;
 		case FLIGHT_MODE:
-			printf("CURRENT MODE: FLIGHT MODE\n");
 			PitchControlMode_HandleFlightTransitions(current_sim_time);
 			break;
 		case FLARE_MODE:
-			printf("CURRENT MODE: FLARE MODE\n");
 			PitchControlMode_HandleFlareTransitions(current_sim_time);
 			break;
 		}
@@ -432,6 +519,7 @@ bool HandleInputCapture(FsContext ctx, int service_id, void* pData)
 	case PANEL_SERVICE_PRE_DRAW:
 	{
 		SimConnect_CallDispatch(hSimConnect, OnInputCaptureEvent, nullptr);
+		printf("X: %lf, Y: %lf, Z: %lf\n", user_input.yoke_x, user_input.yoke_y, user_input.rudder);
 		return true;
 	}
 	break;
@@ -463,8 +551,57 @@ struct CONTROL_SURFACES_DATA
 
 enum DEFINITION_ID
 {
-	CONTROL_SURFACES_DEFINITION
+	CONTROL_SURFACES_DEFINITION,
+	//CONTROL_SURFACES_ANIMATION_DEFINITION
 };
+
+double FlightControlSystem_InferUserLoadFactorIntent()
+{
+	// Get the user stick position with a null zone
+	auto null_zone_error = 0.05 * 2; // 5% of the stick movement is null (2 units of total travel distance over [-1,1])
+	double normalized_user_input = abs(user_input.yoke_y) < null_zone_error ? 0 : user_input.yoke_y;
+
+	// Determine the normal load factor at our bank angle
+	auto roll_angle = aircraft_varget(sim_vars.plane_bank_degrees, sim_vars.radians_units, 0);
+	auto normal_load_factor = 1/cos(roll_angle);
+
+	// Determine the user's load factor
+	auto requested_load_factor = 2 * normalized_user_input + normal_load_factor; // Linear relationship between request and actual value
+
+	// Clamp the load factor to within limits (-1G to +2.5G)
+	double final_load_factor;
+	if (requested_load_factor < -1)
+	{
+		final_load_factor = -1;
+	}
+	else if (requested_load_factor > 2.5)
+	{
+		final_load_factor = 2.5;
+	}
+	else
+	{
+		final_load_factor = requested_load_factor;
+	}
+	
+	printf("Input: %lf, Normal input: %lf, Roll: %lf, Normal LF: %lf, Requested LF: %lf, Final LF: %lf\n", user_input.yoke_y, normalized_user_input, roll_angle, normal_load_factor, requested_load_factor, final_load_factor);
+	return final_load_factor;
+}
+
+static PIDController pitch_controller(-1, 1, 0.5, 0.1, 0.1);
+void FlightControlSystem_ManagePitchControl(double dt)
+{
+	if (pitch_control_mode == FLIGHT_MODE)
+	{
+		auto current_load_factor = aircraft_varget(sim_vars.gforce, sim_vars.gforce_units, 0);
+		auto requested_load_factor = FlightControlSystem_InferUserLoadFactorIntent();
+		control_surfaces.elevator = pitch_controller.Update(requested_load_factor - current_load_factor, dt);
+		printf("Current LF: %lf, Requested LF: %lf, Error LF: %lf, Elevator Position: %lf\n", current_load_factor, requested_load_factor, requested_load_factor - current_load_factor, control_surfaces.elevator);
+	} else
+	{
+		pitch_controller.Reset();
+		control_surfaces.elevator = user_input.yoke_y;
+	}
+}
 
 bool HandleControlSurfaces(FsContext ctx, int service_id, void* pData)
 {
@@ -485,10 +622,16 @@ bool HandleControlSurfaces(FsContext ctx, int service_id, void* pData)
 	break;
 	case PANEL_SERVICE_PRE_DRAW:
 	{
-		control_surfaces.elevator = user_input.yoke_y;
+		// Sent before the gauge is drawn. The pData parameter points to a sGaugeDrawData structure:
+        // - The t member gives the absolute simulation time.
+        // - The dt member gives the time elapsed since last frame.
+		auto* p_draw_data = static_cast<sGaugeDrawData*>(pData);
+		auto dt = p_draw_data->dt;
+		FlightControlSystem_ManagePitchControl(dt);
 		control_surfaces.aileron = user_input.yoke_x;
 		control_surfaces.rudder = user_input.rudder;
 		SimConnect_SetDataOnSimObject(hSimConnect, CONTROL_SURFACES_DEFINITION, SIMCONNECT_OBJECT_ID_USER, 0, 0, sizeof(control_surfaces), &control_surfaces);
+		printf("Elevator deflection: %lf, Elevator Deflection Pct: %lf\n", aircraft_varget(sim_vars.elevator_deflection, sim_vars.degrees_units, 0), aircraft_varget(sim_vars.elevator_deflection_pct, sim_vars.percent_units, 0));
 		return true;
 	}
 	break;
@@ -515,13 +658,13 @@ extern "C"
 	 */
 	MSFS_CALLBACK bool A32NX_gauge_callback(FsContext ctx, int service_id, void* pData)
 	{
-		auto ret = false;
-		ret |= HandleSimVars(ctx, service_id, pData);
-		ret |= HandleSimConnect(ctx, service_id, pData);
-		ret |= HandleFlightControlLaws(ctx, service_id, pData);
-		ret |= HandlePitchControlMode(ctx, service_id, pData);
-		ret |= HandleInputCapture(ctx, service_id, pData);
-		ret |= HandleControlSurfaces(ctx, service_id, pData);
+		auto ret = true;
+		ret &= HandleSimVars(ctx, service_id, pData);
+		ret &= HandleSimConnect(ctx, service_id, pData);
+		ret &= HandleFlightControlLaws(ctx, service_id, pData);
+		ret &= HandlePitchControlMode(ctx, service_id, pData);
+		ret &= HandleInputCapture(ctx, service_id, pData);
+		ret &= HandleControlSurfaces(ctx, service_id, pData);
 		return ret;
 	}
 }
